d43735d55ac1128bfecbc809ecc8e31d69b1a337
Samandar <samandarx6ja@gmail.com>
2022-04-13 12:56:12 +0500

    banner add 1

--- src/component/Home/Homepage.js
+++ src/component/Home/Homepage.js
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
@@ -6,20 +7,24 @@ import 'swiper/css/scrollbar'
 
 const data = [
   {
-    id:1,
-    title: "Movie Name 1",
-    img:""
+    id: 1,
+    title: ' Movie Name 1',
+    img:
+      'https://tejedd76pluu.merlincdn.net//files/slides/banner-image-f61193cf-08fe-4bef-92a4-ba5b6b826828.jpg',
   },
   {
-    id:2,
-    title: "Movie Name 2"
-  }
+    id: 2,
+    title: ' Movie Name 2',
+    img:
+      'https://tejedd76pluu.merlincdn.net//files/slides/banner-image-ce4c404f-36c5-4387-abc7-79b155fd8f1d.jpg',
+  },
 ]
 
 const Homepage = () => {
   return (
     <div className="home">
       <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={50}
         slidesPerView={1}
         navigation
@@ -28,10 +33,19 @@ const Homepage = () => {
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
       >
-        {data.map( item => (
+        {data.map((item) => (
           <SwiperSlide key={item.id}>
-            <div>
-              {item.title}
+            <div className="banner__slide">
+              <img src={item.img} alt="" />
+              <div className="liner__gradient"></div>
+              <div className="banner__desc">
+                <div className="banner__tile">{item.title}</div>
+                <div className="banner__trailer">
+                  <a href="#">
+                    <i className="fas fa-play"></i> Play Movie{' '}
+                  </a>
+                </div>
+              </div>
             </div>
           </SwiperSlide>
         ))}
